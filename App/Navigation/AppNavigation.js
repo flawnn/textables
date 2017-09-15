3437d25273e22310e6e147e6f8d052f6ff9d4bcc
Baran Peters <baran.peters@lessing-ffm.net>
2017-09-15 21:27:02 +0200

    initial

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import MemeScreen from '../Containers/MemeScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  MemeScreen: { screen: MemeScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'MemeScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
